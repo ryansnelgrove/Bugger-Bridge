d4c958e28c011bb6ea5a923c799e8e41fb7e59e3
ryansnelgrove <snelgror@gmatc.matc.edu>
2015-10-01 20:06:26 -0500

    Previous build of the game
    Moved old project files into git repository

---
+++ BuggerBridge/Assets/GameSetup.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class GameSetup : MonoBehaviour {
+    [SerializeField]
+    Slider numberSlider;
+
+    [SerializeField]
+    Text numberDisplay;
+
+    [SerializeField]
+    int numberOfPlayers;
+
+    [SerializeField]
+    Canvas gameCanvas;
+
+    [SerializeField]
+    RectTransform nameFields;
+
+    [SerializeField]
+    InputField inputPrefab;
+
+    string[] names;
+
+    InputField[] extraNames = new InputField[0];
+
+
+    void Update() {
+        numberOfPlayers = (int)numberSlider.value;
+        numberDisplay.text = numberOfPlayers.ToString();
+    }
+
+    public void StartGame()
+    {
+        FillNameArray();
+
+        foreach (string name in names)
+        {
+            if (name == "")
+            {
+                return;
+            }
+        }
+
+        ScoreManager manager = gameCanvas.gameObject.GetComponent<ScoreManager>();
+
+        gameObject.SetActive(false);
+        manager.gameObject.SetActive(true);
+
+        manager.Initialize(numberOfPlayers, names);
+    }
+
+    public void AdjustNames()
+    {
+        numberOfPlayers = (int)numberSlider.value;
+
+        int numberNow = GameObject.FindGameObjectsWithTag("Name").Length;
+        int numberNeeded = numberOfPlayers - numberNow;
+
+        if (numberNeeded > 0)
+        {
+            InputField[] tempStorage = extraNames;
+            extraNames = new InputField[numberOfPlayers - 3];
+
+            System.Array.Copy(tempStorage, 0, extraNames, 0, tempStorage.Length);
+
+            names = new string[numberOfPlayers];
+
+            //extraNames = new InputField[numberNeeded];
+            for (int i = 0; i < extraNames.Length; i++)
+            {
+                if (extraNames[i] == null)
+                {
+                    InputField newField = (InputField)Instantiate(inputPrefab, Vector3.zero, Quaternion.identity);
+                    newField.transform.SetParent(nameFields, false);
+                    newField.transform.localPosition = new Vector3(0, -100 - i * 25, 0);
+                    extraNames[i] = newField;
+                }
+            }
+        }
+        else if (numberNeeded < 0)
+        {
+            InputField[] tempStorage = new InputField[numberOfPlayers - 3];
+            System.Array.Copy(extraNames, 0, tempStorage, 0, tempStorage.Length);
+
+            int length = extraNames.Length;
+            for (int a = numberNeeded; a < 0; a++)
+            {
+                Destroy(extraNames[length + a].gameObject);
+            }
+
+            extraNames = tempStorage;
+        }
+
+        FillNameArray();
+    }
+
+    void FillNameArray()
+    {
+        GameObject[] nameInputFields = GameObject.FindGameObjectsWithTag("Name");
+        names = new string[nameInputFields.Length];
+
+        for (int i = names.Length - 1; i >= 0; i--)
+        {
+            names[i] = nameInputFields[i].GetComponent<InputField>().text;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ BuggerBridge/Assets/Player.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Player
+{
+    public int playerNum;
+    public int[] bidArray;
+    public bool[] bidXarray;
+    int handNumber;
+
+    string nameStr;
+
+    ScoreManager gameManager;
+
+    public string NameStr
+    {
+        get { return nameStr; }
+        set { nameStr = value; }
+    }
+
+    public string NameShort
+    {
+        get { return nameStr.Substring(0, 2).ToUpper(); }
+    }
+
+    public Player(ScoreManager mng, string playerName, int numberOfHands)
+    {
+        gameManager = mng;
+        nameStr = playerName;
+
+        handNumber = numberOfHands;
+
+        ResetPlayer();
+    }
+
+    public void setBid(int handNumber, int bid)
+    {
+        //gameManager.bids[handNumber - 1, playerNum] = bid;
+        bidArray[handNumber - 1] = bid;
+    }
+
+    public int getScore()
+    {
+        int score = 0;
+        for (int i = 0; i < bidXarray.Length; i++)
+        {
+            if (bidXarray[i] == true)
+                score++;
+        }
+        return score;
+    }
+
+    public void ResetPlayer()
+    {
+        bidArray = new int[handNumber];
+        bidXarray = new bool[handNumber];
+    }
+}
